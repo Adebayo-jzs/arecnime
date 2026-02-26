b3909b8e4e33b95329a1a592ef0b726179773324
Adebayo <adedejiadebayo732@gmail.com>
2026-02-26 23:57:57 +0100

    Header: display watchlist count from context

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
 "use client"
-import { Search } from "@mui/icons-material";
+import { Search ,Bookmarks} from "@mui/icons-material";
 import React, { useState, useEffect } from 'react';
 import Link from "next/link";
+import { useWatchlist } from "@/app/watchlist/FavContext";
+
 export default function Header(){
     const [scrolled, setScrolled] = useState(false);
     useEffect(() => {
@@ -11,13 +13,14 @@ export default function Header(){
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+  const {watchlistItems} = useWatchlist();
 
 
   // ${open ? "bg-transparent border-none   backdrop-blur-none px-5" : ""}
     return(
     // <header className="fixed top-4 z-50 left-0 right-0  max-w-[1600px] mx-auto    ">
 
-    <nav className={`  fixed top-0 z-50 left-0 right-0  mx-auto  px-10 py-5 transition-all   text-white duration-300 ease-in-out
+    <nav className={`  fixed top-0 z-50 left-0 right-0  mx-auto  px-5 md:px-10 py-5 transition-all   text-white duration-300 ease-in-out
       ${scrolled ? 'bg-black backdrop-blur-md border border-white/10 shadow-lg px-5 text-black' : 'bg-transparent text-whte border border-transparent'}
       `}>
       <div className="flex items-center justify-between">
@@ -36,12 +39,27 @@ export default function Header(){
         </div> */}
 
         {/* CTA Button */}
+        <div className="flex gap-3 items-center">
+
         <Link
           href="/search" 
           className=" block      text-sm font-semibold transition"
-        >
+          >
           <Search/>
         </Link>
+        <Link href="/watchlist" className="block p-2 rounded-full transition relative">
+          <Bookmarks />
+          <div className="absolute top-0">
+            {/* {cartItems.length > 0 && ( */}
+            <span>
+              <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                {watchlistItems.length || 0}
+              </span>
+            </span>
+            {/* )} */}
+          </div>
+        </Link>
+        </div>
 
         {/* Mobile Menu Icon (Placeholder) */}
          

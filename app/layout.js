4b7137baf8b91ad4061cefac37393f5f2ac4bb5b
Adebayo <adedejiadebayo732@gmail.com>
2026-02-26 23:57:20 +0100

    layout: wrapped app with WatchlistProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { WatchlistProvider } from "./watchlist/FavContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -61,8 +62,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
-        {children}
+        <WatchlistProvider>
+          <Header />
+          {children}
+        </WatchlistProvider>
       </body>
     </html>
   );
